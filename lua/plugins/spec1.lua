f369b2a6df2b4c5851f5a02a99291ecdb61ce638
Achyuth Varma <spenmetcha@chewy.com>
2026-01-14 14:23:22 -0500

    Added new themes

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -1,15 +1,49 @@
 return {
-  -- The ColorScheme should be available when starting Neovim
+  -- Themes
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", lazy = false },
+  { "rose-pine/neovim", name = "rose-pine", lazy = false },
+
+  -- Theme setup and toggle
   {
-    "folke/tokyonight.nvim",
+    "nvim-lua/plenary.nvim",  -- Just using this as a hook to run config
     lazy = false,
-    priority = 1000,
-    opts = {
-      style = "storm",  -- Light theme (options: storm, moon, night, day)
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd("colorscheme tokyonight")
+    priority = 999,
+    config = function()
+      -- Available themes (dark, light)
+      local themes = {
+        dark = { "tokyonight-storm", "catppuccin-mocha", "rose-pine-main" },
+        light = { "tokyonight-day", "catppuccin-latte", "rose-pine-dawn" },
+      }
+      
+      local current_mode = "dark"
+      local current_index = 1
+
+      -- Set initial theme
+      vim.cmd("colorscheme " .. themes[current_mode][current_index])
+
+      -- Fix cursor line for light/dark
+      local function fix_cursorline()
+        if current_mode == "light" then
+          vim.api.nvim_set_hl(0, "CursorLine", { bg = "#e0e0e0" })
+        end
+      end
+
+      -- Toggle light/dark
+      vim.keymap.set("n", "<leader>tt", function()
+        current_mode = current_mode == "dark" and "light" or "dark"
+        vim.cmd("colorscheme " .. themes[current_mode][current_index])
+        fix_cursorline()
+        print("Theme: " .. themes[current_mode][current_index])
+      end, { desc = "Toggle light/dark theme" })
+
+      -- Cycle through themes
+      vim.keymap.set("n", "<leader>tn", function()
+        current_index = current_index % #themes[current_mode] + 1
+        vim.cmd("colorscheme " .. themes[current_mode][current_index])
+        fix_cursorline()
+        print("Theme: " .. themes[current_mode][current_index])
+      end, { desc = "Next theme" })
     end,
   },
 }
