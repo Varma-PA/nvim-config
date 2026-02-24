6f7777adb50518e025e8dc1df929a4c374b194e6
Achyuth Varma <spenmetcha@chewy.com>
2026-02-24 10:03:39 -0500

    Prettier sync

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,14 @@
 return {
   "stevearc/conform.nvim",
   config = function()
+    local util = require("conform.util")
+    -- Project root where Prettier config lives (same resolution as Cursor)
+    local prettier_config_files = {
+      ".prettierrc", ".prettierrc.json", ".prettierrc.yml", ".prettierrc.yaml",
+      ".prettierrc.js", ".prettierrc.cjs", ".prettierrc.mjs", ".prettierrc.toml",
+      "prettier.config.js", "prettier.config.cjs", "prettier.config.mjs",
+      "package.json",
+    }
     require("conform").setup({
       formatters_by_ft = {
         javascript = { "prettier" },
@@ -17,6 +25,9 @@ return {
         prettier = {
           command = "npx",
           args = { "prettier", "--stdin-filepath", "$FILENAME" },
+          cwd = util.root_file(prettier_config_files),
+          -- If no config found, still run from file's dir so npx finds project's Prettier
+          require_cwd = false,
         },
       },
       format_on_save = {
